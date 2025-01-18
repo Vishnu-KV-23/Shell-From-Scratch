62fe0a9d67110811e9951ac0deacf40d32be125d
Vishnu K V <wishnukv2349@gmail.com>
2025-01-19 03:24:23 +0530

    added the Read-Eval-Print Loop (REPL) functionality

--- app/main.py
+++ app/main.py
@@ -2,12 +2,16 @@ import sys
 
 
 def main():
-    # Uncomment this block to pass the first stage
-    sys.stdout.write("$ ")
-
     # Wait for user input
-    command=input()
-    print(f"{command}: command not found")
+    flag=1
+    while(flag):
+        sys.stdout.write("$ ")
+        command=input()
+        while not command:
+            #sys.stdout.write("$ ")
+            flag=0
+            
+        print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
