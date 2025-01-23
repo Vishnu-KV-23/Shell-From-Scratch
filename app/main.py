783ff4c4fad4641aadbc3a4041447a6063e885cd
Vishnu K V <wishnukv2349@gmail.com>
2025-01-23 20:14:37 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,12 +1,15 @@
 import sys
-
+import os
 
 def main():
     # Wait for user input
     commands=['echo','exit','type']
     while(True):
         sys.stdout.write("$ ")
+        flag=0
         command=input()
+        if not command:
+            continue
         match command.split():
             case ['exit','0']:
                 sys.exit(0)
@@ -16,7 +19,16 @@ def main():
                 if arg in commands:
                     print(f"{arg} is a shell builtin")
                 else:
-                    print(f"{arg}: not found")
+                    paths=os.getenv("PATH").split(":")
+                    for path in paths:
+                        if os.path.exists(f"{path}/{arg}"):
+                            flag=1
+                            print(f"{arg} is {path}/{arg}")
+                            break
+                
+                    
+                    if (flag==0):
+                        print(f"{arg}: not found")
             case _:
                 print(f"{command}: command not found")
 
